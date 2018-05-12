b7cb4058d691021cf053af652353b2081210c33d
wisnono <guozy0103@outlook.com>
2018-05-12 16:51:39 +0800

    Create an expresshandlebars instance to use the view engine

--- myapp/app.js
+++ myapp/app.js
@@ -8,19 +8,22 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var handlebars = require('handlebars');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
+var hbs = exphbs.create({ /* config */ });
 
 var app = express();
 
 // view engine setup
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars',hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,12 +31,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
