30cb5ba5248cedd74626f03a2110dc4dcb8febb1
wisnono <guozy0103@outlook.com>
2018-05-12 15:08:35 +0800

    Add forever Change view engine to Handlebars

--- myapp/app.js
+++ myapp/app.js
@@ -7,11 +7,12 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var handlebars = require('handlebars');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'handlebars');
 
 app.use(logger('dev'));
 app.use(express.json());
