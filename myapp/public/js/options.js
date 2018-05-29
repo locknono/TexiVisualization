d601ebd9d764c6593845091027304f9efa2e0794
wisnono <guozy0103@outlook.com>
2018-05-29 18:43:09 +0800

    Options.js IIFE

--- myapp/public/js/options.js
+++ myapp/public/js/options.js
@@ -1,7 +1,10 @@
-var options={
-    normal_opacity:0.6,
-    mouseover_opacity:1,
-    suspending_outer_color:"#D6BD3E",
-    suspending_inner_color:"#CEDDE8",
-    pieview_colorscale:d3.interpolateYlGnBu,
-}
+var options = (function () {
+    return {
+        normal_opacity: 0.6,
+        mouseover_opacity: 1,
+        suspending_outer_color: "#D6BD3E",
+        suspending_inner_color: "#CEDDE8",
+        pieview_colorscale: d3.interpolateYlGnBu,
+    }
+}())
+
