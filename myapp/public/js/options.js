2496ba9ebd9cb771aba0f213fd6825d801f51c9b
wisnono <guozy0103@outlook.com>
2018-05-29 19:03:48 +0800

    Add color numbers in areaScale

--- myapp/public/js/options.js
+++ myapp/public/js/options.js
@@ -1,7 +1,7 @@
 var options = (function () {
 
     var areaScale = d3.scaleOrdinal().domain([])
-        .range(['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99', '#e31a1c', '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a', '#ffff99', '#b15928'])
+        .range(['#a6cee3', '#1f78b4', '#b2df8a', '#ffff33', '#33a02c', '#fb9a99', '#e31a1c', '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a', '#ffff99', '#b15928'])
     var classScale = d3.scale.category20();
     return {
         normal_opacity: 1,
