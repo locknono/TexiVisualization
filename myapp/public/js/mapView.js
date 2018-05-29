8850798483a32baf67bce8fb182415901826434f
wisnono <guozy0103@outlook.com>
2018-05-29 13:38:18 +0800

    Change color Scale

--- myapp/public/js/mapView.js
+++ myapp/public/js/mapView.js
@@ -43,7 +43,7 @@ var mapView = (function () {
 
     })
     var classScale = d3.scaleOrdinal()
-        .range(['#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf', '#999999', '#e41a1c', ]);
+        .range(['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928']);
 
     var d3Overlay = L.d3SvgOverlay(function (selection, projection) {
         //addHexagonBorder(selection, projection);
